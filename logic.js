e236a2b270f7d6a31c7d526ac1c29fbd1c0e3521
mohd-amaan2909 <mohdamaan2909@gmail.com>
2025-08-03 21:51:49 +0530

    Added sum function

--- logic.js
+++ logic.js
@@ -16,5 +16,38 @@ const button2 = document.querySelectorAll(".button2");
            display.innerText += (val2);
         });
 });
+const num = document.querySelectorAll("#num");
+num.addEventListener("click", ()=>{
+      let value = num.innerText;
+      return value;
+})
+
+let numButtons = document.querySelectorAll(".num");
+
+let currentInput = "";
+let firstNum = "";
+let operator = "";
+
+numButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    currentInput += button.innerText;
+    display.innerText = currentInput;
+  });
+});
+
+document.querySelector("#plus").addEventListener("click", () => {
+  firstNum = currentInput;
+  operator = "+";
+  currentInput = "";
+});
+
+document.querySelector("#equal").addEventListener("click", () => {
+  if (operator === "+") {
+    let result = Number(firstNum) + Number(currentInput);
+    display.innerText = result;
+    currentInput = result.toString(); // store result for chaining
+  }
+});
+
 
 
