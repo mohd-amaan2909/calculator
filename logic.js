190bc448d25f5fbf9decd0b7df0b1a2f176c134d
mohd-amaan2909 <mohdamaan2909@gmail.com>
2025-08-09 23:40:28 +0530

    Added more mathematical functions

--- logic.js
+++ logic.js
@@ -5,6 +5,9 @@ const buttons = document.querySelectorAll(".button1 , .button2");
 buttons.forEach(button =>{
 button.addEventListener("click", ()=> {
        const value = button.innerText;
+function degToRad(deg) {
+        return deg * (Math.PI / 180);
+        }
 
       if(value==="AC"){
         display.innerText="";
@@ -14,7 +17,44 @@ button.addEventListener("click", ()=> {
         }catch{
           display.innerText="error";
         }
-      }else{
+      } else if (value === "sin") {
+        display.innerText = Math.sin(degToRad(Number(display.innerText)));
+      } else if( value ==="cos"){
+        display.innerText = Math.cos(degToRad(Number(display.innerText)));
+      } else if( value ==="tan"){
+        display.innerText = Math.tan(degToRad(Number(display.innerText)));
+      } else if( value ==="sinh"){
+        display.innerText = Math.sinh(degToRad(Number(display.innerText)));
+      } else if( value ==="cosh"){
+        display.innerText = Math.cosh(degToRad(Number(display.innerText)));
+      } else if( value ==="tanh"){
+        display.innerText = Math.tanh(degToRad(Number(display.innerText)));
+      } else if( value ==="Rand"){
+        display.innerText = Math.random();
+      }else if( value ==="π"){
+        display.innerText *= Math.PI;
+      } else if( value === "Deg"){
+        function degToRad(deg) {
+        return deg * (Math.PI / 180);
+        }
+      } else if( value ==="e"){
+        display.innerText = Math.E;
+      } else if( value === "1/x"){
+        let num = parseFloat(display.innerText);
+        if(num===0){
+          display.innerText = "error";
+        }else{
+          display.innerText = 1/num;
+        }
+      } else if( value === "x1/2"){
+        let num = parseFloat(display.innerText);
+        if(num===0){
+          display.innerText = "0";
+        } else{
+          display.innerText = Math.sqrt(num);
+        }
+      }
+      else{
         display.innerText +=value;
       }
 });
