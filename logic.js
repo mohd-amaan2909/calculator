bb94d5c2989c2ac2c4f998467f8a89c983a42a3a
mohd-amaan2909 <mohdamaan2909@gmail.com>
2025-08-09 18:50:05 +0530

    Added basic Arthemetic operations

--- logic.js
+++ logic.js
@@ -1,53 +1,21 @@
 const display = document.querySelector(".display");
-const button1 = document.querySelectorAll(".button1");
-const button2 = document.querySelectorAll(".button2");
+const buttons = document.querySelectorAll(".button1 , .button2");
 
- button1.forEach(button1 =>{
-    button1.addEventListener("click" , ()=>{
-          let val1= button1.innerText;
-           console.log( val1);
-           display.innerText += (val1);
-        });
+
+buttons.forEach(button =>{
+button.addEventListener("click", ()=> {
+       const value = button.innerText;
+
+      if(value==="AC"){
+        display.innerText="";
+      }else if(value==="="){
+        try{
+          display.innerText = eval(display.innerText);
+        }catch{
+          display.innerText="error";
+        }
+      }else{
+        display.innerText +=value;
+      }
 });
- button2.forEach(button2 =>{
-    button2.addEventListener("click" , ()=>{
-          let val2= button2.innerText;
-           console.log( val2);
-           display.innerText += (val2);
-        });
 });
-const num = document.querySelectorAll("#num");
-num.addEventListener("click", ()=>{
-      let value = num.innerText;
-      return value;
-})
-
-let numButtons = document.querySelectorAll(".num");
-
-let currentInput = "";
-let firstNum = "";
-let operator = "";
-
-numButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    currentInput += button.innerText;
-    display.innerText = currentInput;
-  });
-});
-
-document.querySelector("#plus").addEventListener("click", () => {
-  firstNum = currentInput;
-  operator = "+";
-  currentInput = "";
-});
-
-document.querySelector("#equal").addEventListener("click", () => {
-  if (operator === "+") {
-    let result = Number(firstNum) + Number(currentInput);
-    display.innerText = result;
-    currentInput = result.toString(); // store result for chaining
-  }
-});
-
-
-
